3c1ec4cd93606e037a9953009159a05070d66509
Javkhlan Enkhbold <javkhlan.enkhbold@campus.tu-berlin.de>
2022-06-14 19:43:36 +0200

    add last entry value

--- gas_meter.py
+++ gas_meter.py
@@ -3,21 +3,35 @@ from urllib.request import urlopen
 import argparse
 import sys
 from influxdb import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 from RPi import GPIO
 from time import sleep
 from datetime import datetime
+import json 
 
 def send_to_influx(counter):
         client = InfluxDBClient(host='localhost', port=8086, username='gas_meter' , password='grafana', database='gas_meter')
         data = [
                                 {
                                         "measurement": "impulse",
-                                        "time": datetime.now(),
+                                        "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
                                         "fields": {"count": counter},
                                 }
                         ]
         print(data)
         client.write_points(data)
+
+def query_last_entry():
+    client = InfluxDBClient(host="localhost", port=8086, username="gas_meter", password="grafana", database="gas_meter")
+    value = client.query(("SELECT LAST(*) FROM impulse"))
+    time = list(value)[0][0]["time"]
+    last_counter = list(value)[0][0]["last_counter"]
+    date_time_obj = datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ')
+    time_delta = datetime.utcnow()-date_time_obj
+    total_seconds = time_delta.total_seconds()
+    minutes = divmod(total_seconds, 60)[0]
+    print("last entry: " + str(minutes))
+    return minutes, last_counter
 
 def check_internet_on():
     try:
@@ -25,11 +39,11 @@ def check_internet_on():
         status = "Connected"
     except:
         status = "Not connected, reconnecting..."
-        os.system("sudo ifconfig wlan0 up")        
+        os.system("sudo ifconfig wlan0 up")
         sleep(8)
         print(os.system("iwconfig"))
     print("Internet connection: "+ status)
-
+    
 def wait_for_edge():
     GPIO.wait_for_edge(11, GPIO.RISING)
     time = datetime.now()
@@ -38,17 +52,21 @@ def wait_for_edge():
     return time
 
 def read_counter():
-    with open("counter.txt") as file:
+    with open("counter.txt", "r") as file:
         lines = file.read().splitlines()
         last_line = lines[-1]
         print(last_line)
-        file.close()
         return last_line
 
 def write_counter_into_file(counter):
     with open("counter.txt","w") as file:
          file.write(str(counter))
-         file.close()
+
+def update_counter(counter):
+    counter = int(counter)
+    counter += 1 
+    write_counter_into_file(counter)
+    return counter
 
 def check_if_impuls_high(GPIO_PIN_Number, time_before_rise, counter):
     if GPIO_PIN_Number == GPIO.HIGH:
@@ -58,12 +76,10 @@ def check_if_impuls_high(GPIO_PIN_Number, time_before_rise, counter):
         print("Falling edge:", time)
         print("Impuls length: ", time-time_before_rise)
         sleep(1)
-        counter = int(counter)+1
-        print(counter)
-        write_counter_into_file(counter)
+        new_counter = update_counter(counter)
         sleep(2)
-        print("Impuls:", int(counter))
-        send_to_influx((counter))
+        print("Impuls:", int(new_counter))
+        send_to_influx((new_counter))
         sleep(3)
         print("Waiting for new impuls")
     else:
@@ -75,11 +91,13 @@ check_internet_on()
 #GPIO connection setup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#Initialisation counter
-counter = read_counter()
+time, counter = query_last_entry()
 
 while True:
-        check_internet_on()
+        #check_internet_on()
+        counter = read_counter()
         time_before_rising = wait_for_edge()
         check_if_impuls_high(GPIO.input(11), time_before_rising, counter)
+        #if time > 15:
+        #    check_if_impuls_high(GPIO.input(11), time_before_rising, counter)
         continue
