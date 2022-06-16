55af298b9aa8384f31ca71c1621545c27229a4a6
Javkhlan Enkhbold <javkhlan.enkhbold@campus.tu-berlin.de>
2022-06-16 20:26:06 +0200

    bug fix

--- update_influx.py
+++ update_influx.py
@@ -4,7 +4,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from RPi import GPIO
 from time import sleep
 from datetime import datetime
-
+import os
 
 def send_to_influx(counter):
         client = InfluxDBClient(host='localhost', port=8086, username='gas_meter' , password='grafana', database='gas_meter')
@@ -29,11 +29,24 @@ def query_last_entry_time():
     minutes = divmod(total_seconds, 60)[0]
     return minutes, last_counter
 
+def check_internet_on():
+    try:
+        urlopen("https://www.google.com")
+        status = "Connected"
+    except:
+        status = "Not connected, reconnecting..."
+        os.system("sudo ifconfig wlan0 up")
+        sleep(8)
+        print(os.system("iwconfig"))
+    print("Internet connection: "+ status)
+
 while True: 
      #Check every 15Minutes, wheter any changes in the db by checking the last register time in minutes.
+     #Check internet connection.
+     check_internet_on()
      last_register_time, last_counter = query_last_entry_time()
      print("The last query time: " + str(last_register_time))
      print("The last_counter: " + str(last_counter()
      if last_register_time > 15:
          send_to_influx(last_counter)
-     sleep(10)
+     sleep(30)
